2e45bc09bd4bf29aba8d61ba2031764ed49d47fb
Namgyal <sasnams.e.sxd162001@gmail.com>
2025-02-03 14:17:13 -0500

    Initial Commit

---
+++ AirBnbClone/AirBnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbCloneApp.swift
+//  AirBnbClone
+//
+//  Created by Namgyal Thily on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
